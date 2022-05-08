7307bd282d467d0182fc1c80d2c0fcad26129256
JD Mauthe <jd@jdmauthe.com>
2022-05-08 05:21:50 +0000

    added config support

---
+++ src/utils/config.js
@@ -0,0 +1,30 @@
+/**
+ * Gets the config
+ * @return {Object} config
+ */
+function getConfig() {
+  const config = getDefaults();
+  try {
+    Object.assign(config, require('../../config.json'));
+  } catch (err) {}
+
+  return config;
+}
+
+
+/**
+ * Gets the default config
+ * @return {Object} default config
+ */
+function getDefaults() {
+  return {
+    'overwrite': false,
+    'file': 'README.md',
+    'hostnames': {
+      'leetcode': {},
+    },
+    'transformers': {},
+  };
+}
+
+module.exports = getConfig();
